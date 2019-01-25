fb3d1070a2187efc244605672743a7b5c2f727e9
Franklin Parker <fparker@deliveryman.com>
2019-01-24 19:51:06 -0500

    initial commit

---
+++ client/src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SharedModule} from '../shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
+import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    NavHeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule
+  ],
+  exports:[
+    HomeComponent,
+    NavHeaderComponent
+  ]
+})
+export class CoreModule { }
