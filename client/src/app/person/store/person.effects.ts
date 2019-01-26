c528bbb1f93894d04f0187f399ebb96a607832b8
Franklin Parker <fparker@deliveryman.com>
2019-01-26 18:21:51 -0500

    fix

--- client/src/app/person/store/person.effects.ts
+++ client/src/app/person/store/person.effects.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import {Store} from '@ngrx/store';
+
+import {AppState} from '../../reducers';
+import {PersonService} from '../service/person.service';
 
 
 @Injectable()
 export class PersonEffects {
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions,
+              private store: Store<AppState>,
+              private personService: PersonService) {}
 }
